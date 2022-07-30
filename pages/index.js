6116c5a7eee2dc3823d380434c7cee9f1770643b
Juan Manuel Garcia <jmg.utn@gmail.com>
2022-07-30 12:32:44 -0300

    settings

--- pages/index.js
+++ pages/index.js
@@ -111,7 +111,7 @@ export default function Home() {
           }
           </Carousel>
 
-          <div className="pb-0 p-4 mt-2 md:mt-4 text-white text-justify">
+          <div className="mx-auto pb-0 py-2 mt-2 md:mt-4 text-white text-justify" style={{maxWidth: 600}}>
             <div className={showMore ? "" : "line-clamp-6"}>
               {animal.long_description}
             </div>
