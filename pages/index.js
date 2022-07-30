253eacb55792a0bf9209bd99e456393e1ef43c75
Juan Manuel Garcia <jmg.utn@gmail.com>
2022-07-30 16:31:11 -0300

    fixes

--- pages/index.js
+++ pages/index.js
@@ -112,12 +112,12 @@ export default function Home() {
           }
           </Carousel>
 
-          <div className="mx-auto pb-0 py-2 mt-2 md:mt-4 text-white text-justify" style={{maxWidth: 600}}>
+          <div className="mx-auto p-4 md:p-0 pb-0 py-2 mt-2 md:mt-4 text-white text-justify text-lg" style={{maxWidth: 600}}>
             <div className={showMore ? "" : "line-clamp-6"}>
               {animal.long_description}
             </div>
 
-            <button onClick={() => setShowMore(!showMore)} class="mt-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1.5 px-3 rounded">
+            <button onClick={() => setShowMore(!showMore)} class="mt-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 rounded text-xl">
                 Mostrar {!showMore ? "más" : "menos"}
             </button>
           </div>
