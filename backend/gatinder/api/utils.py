bfc0dfaa0ce99bc4ac81b3cf819f776f1cc58e71
Juan Manuel García <jmg.utn@gmail.com>
2022-08-17 01:56:45 -0300

    wait invalidate cache async

--- backend/gatinder/api/utils.py
+++ backend/gatinder/api/utils.py
@@ -1,10 +1,15 @@
+from threading import Thread
 from django.conf import settings
 import requests
+import time
 
+
+def async_invalid_cache():
+
+    time.sleep(1)
+    url = "{}/api/revalidate".format(settings.FRONT_END_APP_URL)
+    response = requests.get(url, headers={"secret": settings.INVALIDATE_CACHE_SECRET_KEY})
 
 def invalidate_cache():
 
-    url = "{}/api/revalidate".format(settings.FRONT_END_APP_URL)
-    print ("Revalidating cache")
-    response = requests.get(url, headers={"secret": settings.INVALIDATE_CACHE_SECRET_KEY})
-    print (response.content)
+    Thread(target=async_invalid_cache).start()
